267b56ab8e30345cd4e65d588a29a9de172b1a82
hendisantika <hendisantika@yahoo.co.id>
2024-12-30 23:20:51 +0700

    Add removeCrossDomainCookie in Rq🫸🌀✏️📗🐧🐳⬆

--- src/main/java/id/my/hendisantika/backend/global/RsData/rq/Rq.java
+++ src/main/java/id/my/hendisantika/backend/global/RsData/rq/Rq.java
@@ -50,4 +50,15 @@ public class Rq {
 
         resp.addHeader("Set-Cookie", cookie.toString());
     }
+
+    public void removeCrossDomainCookie(String name) {
+        ResponseCookie cookie = ResponseCookie.from(name, null)
+                .path("/")
+                .maxAge(0)
+                .sameSite("None")
+                .secure(true)
+                .httpOnly(true)
+                .build();
+        resp.addHeader("Set-Cookie", cookie.toString());
+    }
 }
