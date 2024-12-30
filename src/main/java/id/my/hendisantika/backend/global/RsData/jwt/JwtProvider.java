df843eec9c3aa5ba01221fd0798e6f00e9b8dfce
hendisantika <hendisantika@yahoo.co.id>
2024-12-30 23:18:11 +0700

    Add JwtProvider🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/java/id/my/hendisantika/backend/global/RsData/jwt/JwtProvider.java
@@ -0,0 +1,24 @@
+package id.my.hendisantika.backend.global.RsData.jwt;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : backend
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 30/12/24
+ * Time: 23.17
+ * To change this template use File | Settings | File Templates.
+ */
+@Component
+public class JwtProvider {
+    @Value("${custom.jwt.secretKey}")
+    private String secretKeyOrigin;
+
+    private SecretKey cachedSecretKey;
+}
