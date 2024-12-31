69bbe2ac356f7f9d8a6e0afde16dfa9de47c0d92
hendisantika <hendisantika@yahoo.co.id>
2025-01-01 06:28:12 +0700

    feat: Add article id page.tsx🫸🌀✏️📗🐧🐳⬆

---
+++ frontend/src/app/article/[id]/page.tsx
@@ -0,0 +1,39 @@
+'use client'
+
+import api from '@/utils/api'
+import {useQuery} from '@tanstack/react-query'
+import Link from 'next/link'
+import {useParams} from 'next/navigation'
+
+export default function ArticleDetail() {
+    const params = useParams()
+
+    const getArticle = async () => {
+        return await api.get(`/articles/${params.id}`)
+            .then((response) => response.data.data.article)
+    }
+
+    const {isLoading, error, data} = useQuery({
+        queryKey: ['article'],
+        queryFn: getArticle
+    });
+
+    if (error) {
+        console.log(error)
+    }
+
+    if (isLoading) <>Loading...</>
+
+    if (data) {
+        return (
+            <>
+                <h1>Bulletin Board Details {params.id}th</h1>
+                <div>{data.subject}</div>
+                <div>{data.content}</div>
+                <div>{data.createdDate}</div>
+                <div>{data.modifiedDate}</div>
+                <Link href={`/article/${params.id}/edit`}>Edit</Link>
+            </>
+        )
+    }
+}
