b40c4b93392c35f563673f0cc48e63b226c31e43
hendisantika <hendisantika@yahoo.co.id>
2025-01-01 06:22:32 +0700

    feat: Add page.tsx🫸🌀✏️📗🐧🐳⬆

---
+++ frontend/src/app/about/page.tsx
@@ -0,0 +1,24 @@
+'use client'
+
+import {useEffect, useState} from 'react'
+import api from '../../utils/api'
+
+export default function About() {
+    const [member, setMember] = useState({})
+
+    useEffect(() => {
+        api.get('/members/me').then((response) =>
+            setMember(response.data.data.member)
+        )
+    }, [])
+
+    return (
+        <>
+            <h1>Introduction Page</h1>
+            <ul>
+                <li>id : {member.id}</li>
+                <li>username : {member.username}</li>
+            </ul>
+        </>
+    )
+}
